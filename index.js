5ca5a02650b8bb6e4523c4f643a26cbc28fa3bbb
A.J. Romaniello <ajiellodev@gmail.com>
2021-11-08 18:54:58 -0700

    Completed.

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 function findMinAndRemove(array){
+    let min = array[0]
 
+    let i = 0
+    array.forEach((e, idx) => {
+        if (e < min) {
+            min = e
+            i = idx
+        }
+    })
+
+    array.splice(i, 1)
+    return min
 }
 
 function selectionSort(array){
 
+    let sorted = []
+    let min = array[0]
+
+    while (array.length !== 0) {
+        min = findMinAndRemove(array)
+        sorted.push(min)
+    }
+
+    return sorted
 }
